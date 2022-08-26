c73e93cf36edbd26449239d4083eb32a8773ce22
Sergio Valadez <sergioval@gmail.com>
2022-08-26 14:59:46 -0500

    Add public dir to save images

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
+import logo from '../../public/logo.png';
 
 const Navbar = () => {
   return (
     <nav className='navbar' >
       <ul className='nav'>
-        <img src='logo' alt='logo' />
+        <img src={logo} size='8' alt='logo' />
         <li>
           <Link href="/">Inicio</Link>
         </li>
