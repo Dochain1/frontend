a7183ac8460fa0cd1d1c807e4558239eb7e4f025
IvySaskia <ivysaskia@gmail.com>
2022-08-31 12:37:13 -0400

    [UPDATE] Style for case card

--- components/CaseCard/index.jsx
+++ components/CaseCard/index.jsx
@@ -7,17 +7,17 @@ const CaseCard = ({ item }) => {
   return<>  
   <div className="shadow-2xl max-w-sm w-full lg:max-w-full lg:flex rounded-2xl border-r border-b border-l border-t border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white  p-4 flex flex-col justify-between leading-normal">
     
-    <div className="text-center pb-2 inline-block bg-gray-200 rounded-full px-3 py-1 ">
-      <span className="text-xl font-semibold text-gray-700 mr-2 mb-2">CBBA5001</span>
+    <div className="text-center pb-2 inline-block bg-blue-200 rounded-full px-3 py-1 ">
+      <span className="text-xl font-semibold text-gray-900 mr-2 mb-2">CBBA5001</span>
     </div>
     
     <div className="flex items-center mt-4">
-      <BsBriefcaseFill  className="mr-2 mb-2"/>
+      <BsBriefcaseFill  className="text-blue-600 mr-2 mb-2"/>
       <div className="text-gray-900 font-bold text-xl mb-2 leading-none">PORTAFOLIO</div>
     </div>
     
     <div className="flex items-start mt-4">
-      <BsUnlockFill  className="fill-current text-gray-500 w-3 h-3 mr-2"/>
+      <BsUnlockFill  className="fill-current text-blue-500 w-3 h-3 mr-2"/>
       <div className="text-sm">
         <p className="text-gray-900 leading-none font-semibold">Estado</p>
         <p className="text-gray-700">Abierto</p>
@@ -25,7 +25,7 @@ const CaseCard = ({ item }) => {
     </div>
 
     <div className="flex items-start mt-4">
-      <FaUserAlt  className="fill-current text-gray-500 w-3 h-3 mr-2"/>
+      <FaUserAlt  className="fill-current text-blue-500 w-3 h-3 mr-2"/>
       <div className="text-sm">
         <p className="text-gray-900 leading-none font-semibold">Demandante</p>
         <p className="text-gray-700">Nombre</p>
@@ -33,7 +33,7 @@ const CaseCard = ({ item }) => {
     </div>
 
     <div className="flex items-start mt-4">
-      <FaUserTie  className="fill-current text-gray-500 w-3 h-3 mr-2"/>
+      <FaUserTie  className="fill-current text-blue-500 w-3 h-3 mr-2"/>
       <div className="text-sm">
         <p className="text-gray-900 leading-none font-semibold">Abogado Demandante</p>
         <p className="text-gray-700">Nombre</p>
@@ -41,7 +41,7 @@ const CaseCard = ({ item }) => {
     </div>
 
     <div className="flex items-start mt-4">
-      <FaUserAlt  className="fill-current text-gray-500 w-3 h-3 mr-2"/>
+      <FaUserAlt  className="fill-current text-blue-500 w-3 h-3 mr-2"/>
       <div className="text-sm">
         <p className="text-gray-900 leading-none font-semibold">Demandado</p>
         <p className="text-gray-700">Nombre</p>
@@ -49,7 +49,7 @@ const CaseCard = ({ item }) => {
     </div>
 
     <div className="flex items-start mt-4">
-      <FaUserTie  className="fill-current text-gray-500 w-3 h-3 mr-2"/>
+      <FaUserTie  className="fill-current text-blue-500 w-3 h-3 mr-2"/>
       <div className="text-sm">
         <p className="text-gray-900 leading-none font-semibold">Abogado Demandado</p>
         <p className="text-gray-700">Nombre</p>
@@ -57,7 +57,7 @@ const CaseCard = ({ item }) => {
     </div>
 
     <div className="flex items-start mt-4">
-      <MdPlace  className="fill-current text-gray-500 w-3 h-3 mr-2"/>
+      <MdPlace  className="fill-current text-blue-500 w-3 h-3 mr-2"/>
       <div className="text-sm">
         <p className="text-gray-900 leading-none font-semibold">Lugar del caso</p>
         <p className="text-gray-600">Cochabamba, Bolivia</p>
