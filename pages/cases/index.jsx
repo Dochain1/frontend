c6fb26dd3d33a034d5eeba054e1cb8a26d1dceab
IvySaskia <ivysaskia@gmail.com>
2022-08-31 12:34:59 -0400

    [ADD] Grid for case cards

--- pages/cases/index.jsx
+++ pages/cases/index.jsx
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import CaseCard from '../../components/CaseCard';
 
 const Cases = () => {
-  const [cases, setCases] = useState(Array(5).fill());
+  const [cases, setCases] = useState(Array(3).fill());
   return (
     <>
-      <h1>Cases</h1>
-      <h3>List of case</h3>
-      {cases.map((_, index) => {
-        return <CaseCard item={index} key={index} />;
-      })}
+      <h1 className='text-center text-black text-2xl my-4'>Casos</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full mx-auto px-20">
+        {cases.map((_, index) => {
+          return<CaseCard item={index} key={index} />;
+        })}
+      </div>
     </>
   );
 };
