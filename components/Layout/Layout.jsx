12f983c2c6ecd8636e6d18077a7bc849512e8378
IvySaskia <ivysaskia@gmail.com>
2022-09-04 10:42:08 -0400

    [ADD] Layout using chakra

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import Footer from '../Footer';
 import Navbar from '../Navbar';
+import {
+  Box,
+  Flex,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Flex minH="100vh" direction="column">
+      <Box
+        mx="auto"
+        maxW={"7xl"}
+        width="100%"
+        bg={useColorModeValue("white", "gray.800")}
+        px={4}
+      >
       <Navbar />
-      {children}
+      </Box>
+      <Box mx="auto" flex={1} p={4} maxW={"7xl"} width="100%">
+        {children}
+      </Box>
       <Footer />
-    </>
+    </Flex>
   );
 };
 
