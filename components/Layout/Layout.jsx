fb2d03b7c7c1f8be33d55bfd72696e0916226a3c
IvySaskia <ivysaskia@gmail.com>
2022-09-08 00:53:41 -0400

    [ADD] Icon and title toclear

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -5,9 +5,16 @@ import {
   Box,
   Flex
 } from "@chakra-ui/react";
+import Head from 'next/head';
+
 
 const Layout = ({ children }) => {
   return (
+    <>
+    <Head>
+      <title>DOCHAIN</title>
+      <link rel="icon" href="./images/logo.png" />
+    </Head>
     <Flex minH="100vh" direction="column">
       <Navbar />
       <Box mx="auto" flex={1} p={4} maxW={"7xl"} width="100%">
@@ -15,6 +22,7 @@ const Layout = ({ children }) => {
       </Box>
       <Footer />
     </Flex>
+    </>
   );
 };
 
